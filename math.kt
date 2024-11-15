40c44bede49e8763bf1ab344b48b6e23c70c1d3f
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-15 19:28:59 +0530

    Create math.kt

---
+++ math.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val num1 = 7
+    val num2 = 8
+    val num3 = 6
+    println(num1 * num2 - num3) // OutPut is 50
+
+    var num4 = 1784
+    var num5 = 8908
+    var num6 = 3422
+    var num7 = 5792
+    var num8 = 4786
+    println(num4 * num5 / num6 + num7 - num8) // OutPut is 5650
+}
