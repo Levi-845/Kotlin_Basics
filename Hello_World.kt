44bf33e0b83855a50b1868ed0e1bddbb3c571ad5
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-15 19:18:00 +0530

    created Hello_World.kt

---
+++ Hello_World.kt
@@ -0,0 +1,10 @@
+fun main () {
+    println("Hello World.") // Hello World program 
+    // OutPut :- Hello World.
+
+    println("------------") // serarater
+
+    var firstProgram = "Hello World" // i made Hello World variable and then print it.  
+    println(firstProgram)
+    // OutPut :- Hello World
+}
