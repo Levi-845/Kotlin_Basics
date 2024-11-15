67643db6b2975a270b70e321c774db29933ed73b
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-15 19:21:17 +0530

    Create variable.kt

---
+++ variable.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+var myFirstName: String = "ahmed" // we can assign var variable value.
+println(myFirstName) // it is optinal! we can use DataType name. 
+
+val middleName = "muhmmad" // we can assign val variable value too.
+println(middleName) // compilar can detect DataType by value of variable.
+
+var lastName = "naviwala"
+println(lastName)
+
+val age: Int = 20 // double quotes ("") is use for strings variables, integer number didn't need double quotes (""). 
+println(age)
+
+var year = 2024 // we can use string in println("so we can write something"). 
+println("year is $year") // if we want to println variable with string so we have to use ($)dollar sign with variable.
+
+var boolean1 = true // true and false are boolean, so they can't be in double quotes.
+println(boolean1) // if we use double quotes, then it become string.
+
+val boolean2 = false // true and false are boolean, so they can't be in double quotes.
+println(boolean2) // if we use double quotes, then it become string.
+
+}
