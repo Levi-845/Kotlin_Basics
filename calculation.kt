5fe8cacf9ac454afd7a8c5d3c5eae0e9f0954bb4
AhmedES247 <154126408+ahmedES247@users.noreply.github.com>
2024-11-15 19:30:24 +0530

    Create calculation.kt

---
+++ calculation.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+var num1 = 10
+var num2 = 15
+println(num1 + num2) // OutPut 25
+
+val num3: Int = 80
+println(num3 - 5) // OutPut 75
+
+val num4 = 7
+println(num4 * 3) // OutPut 21
+
+val num5 = 25
+println(num5 / 5) // OutPut 5
+
+val num6 = 10
+println(num6 % 3) // OutPut 1, (%) modulus operator will display remaining value.
+
+}
